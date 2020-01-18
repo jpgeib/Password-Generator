58c4b6a7ad4ef1a2411f6b47dba877f6a88e0cb2
James Geib <jpgeib2@gmail.com>
2020-01-17 17:28:32 -0800

    password generator now functioning

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 // DOM elements
-const resultEl = document.getElementById('result');
+
+const passwordEl = document.getElementById('password');
 const lengthEl = document.getElementById('length');
 const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
+const specialEl = document.getElementById('special');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
 
+// Generator Events
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
-    num: getRandomNum,
+    number: getRandomNumber,
     special: getRandomSpecial
 };
 
 generateEl.addEventListener('click', () => {
-    const length = length.value;
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSpecial = specialEl.checked;
+    
+    passwordEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSpecial, length);
+});
 
-    console.log(length);
-})
+//Password Function
+
+function generatePassword(lower, upper, number, special, length) {
+    let result = '';
+    const typesCount = lower + upper + number + special;
+    // console.log('typesCount: ', typesCount);
+    const typesArr = [{lower}, {upper}, {number}, {special}].filter(item => Object.values(item)[0]);
+    // console.log('typesArr: ', typesArr);
+
+    if(typesCount === 0) {
+        return '';
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            // console.log('funcName: ', funcName);
+
+            result += randomFunc[funcName]();
+        });
+    }
+
+    const finalPassword = result.slice(0, length);
+    
+    return finalPassword;
+}
+
 
 // Generator functions
 
@@ -31,7 +65,7 @@ function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function getRandomNum() {
+function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
